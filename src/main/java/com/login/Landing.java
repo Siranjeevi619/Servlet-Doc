6011f4c154b7a87bad97e305a91fd172ab0f5af9
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-06-26 10:05:41 +0530

    initial setup

---
+++ src/main/java/com/login/Landing.java
@@ -0,0 +1,14 @@
+package com.login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Landing extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
---
+++ src/main/java/com/login/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.login;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ("admin".equals(username) && "1234".equals(password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("username", username);
+            res.sendRedirect("dashboard");
+        } else {
+            res.getWriter().println("Invalid credentials. <a href='login.html'>Try again</a>");
+        }
+    }
+}
